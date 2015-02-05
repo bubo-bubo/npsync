9b728a39eba7d11807201126879f4b202b18dc74
Alexey Filin <filin@oka07.ihep.su>
2015-02-05 15:09:24 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+npsync is a synchronization utility for storages with non-POSIX interfaces.
+It provides rsync-like functionality easily adapted to exotic interfaces.
+
+See npsync.1 for more info.
+
+Tarball contents:
+
+  - INSTALL -- guide to install npsync
+  - README.md -- the file
+  - DEVELOPER_GUIDE -- some advices to extend/adapt npsync
+  - npsync -- the utility
+  - npsync.1 -- manual
+  - adler32.c -- checksum utility
+# npsync
---
+++ adler32.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <zlib.h>
+
+
+#define BUF_SIZE (1<<20)
+
+/*
+  IO performance is the bottleneck, no super-optimization is required
+  gcc -O6 -Wall adler32.c -o adler32 -lz
+*/
+
+int
+main( int argc, char *argv[] )
+{
+  int fd=-1;
+  u_char buf[BUF_SIZE];
+  ssize_t size=0;
+  u_int32_t adler = adler32( 0L, Z_NULL, 0 );
+  if ( argc != 2 ) {
+    fprintf( stderr, "Usage: %s FILE_NAME\n", argv[0] );
+    exit( 1 );
+  }
+  if ( (fd=open( argv[1], O_RDONLY )) == -1 ) {
+    long e=errno;
+    perror( argv[1] );
+    exit( e );
+  }
+  while ( (size=read( fd, buf, BUF_SIZE )) > 0 ) {
+    adler = adler32( adler, buf, size );
+  }
+  if ( size < 0 ) {
+    long e=errno;
+    perror( argv[1] );
+    exit( e );
+  } else {
+    printf( "%x\n", adler );
+  }
+  close( fd );
+  return 0;
+}
